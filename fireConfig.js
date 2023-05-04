49a5fe7e6fbf6d7840f813d89c6776b9071e393f
landichoo <132576258+landichoo@users.noreply.github.com>
2023-05-04 20:34:49 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBuWKljjgmE9z5_8a__TIfP1oco2KcVI6U",
+    authDomain: "crud-ff44d.firebaseapp.com",
+    databaseURL: "https://crud-ff44d-default-rtdb.firebaseio.com",
+    projectId: "crud-ff44d",
+    storageBucket: "crud-ff44d.appspot.com",
+    messagingSenderId: "301938158245",
+    appId: "1:301938158245:web:cac9fd78af57926b80fb4c",
+    measurementId: "G-4WHQK4WJ3X"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
